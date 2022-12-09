a3b318f28353ca3ae83d5b4c1aa84ae88af2f99b
neadianyanwu <anya.ijeomanwaneadi@gmail.com>
2022-12-08 23:49:00 -0800

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
- * main - prints a given phrase
- * Return: 0 if exited properly, non-zero otherwise
+ * main - Entry
+ * Return: Always 0
  */
 
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	    return (0);
-}
+	return (0);
+}	
